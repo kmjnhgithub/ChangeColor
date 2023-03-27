98dfae98edb2ab2b264ba131b3850746f9d36899
@mike <mike@innotech.me>
2023-03-27 12:47:24 +0800

    random color

---
+++ ChangeColor/UIButton+extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIButton+extension.swift
+//  ChangeColor
+//
+//  Created by mike liu on 2023/3/27.
+//
+
+import Foundation
--- ChangeColor/ViewController.swift
+++ ChangeColor/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var alphaSlider: UISlider!
+    
+    @IBOutlet weak var customRandomButton: UIButton!
+    @IBOutlet weak var anyaImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        customRandomButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func changeColor(_ sender: UISlider) {
 
+        anyaImageView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
+    }
+    
+    
+    @IBAction func randomButton(_ sender: UIButton) {
+        redSlider.value = Float.random(in: 0...1)
+        greenSlider.value = Float.random(in: 0...1)
+        blueSlider.value = Float.random(in: 0...1)
+        alphaSlider.value = Float.random(in: 0...1)
+        anyaImageView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: CGFloat(alphaSlider.value)
+        )
+    }
+    
 }
 
